89dca2afbccf508566fce0468b91df8caa925c7f
kjohna <43828763+kjohna@users.noreply.github.com>
2019-02-01 09:29:52 -0700

    redux-thunk, devtools working

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -14,6 +14,18 @@
  }
 */
 
+const initialState = {
+  smurfs: [
+    {
+      name: 'TESTSMURF',
+      age: 200,
+      height: '5cm'
+    }
+  ],
+  fetchingSmurfs: false,
+  error: null
+}
+
 /*
   You'll only need one smurf reducer for this project.
   Feel free to export it as a default and import as rootReducer. 
@@ -21,3 +33,12 @@
   There is no need for 'combineReducers' in this project.
   Components can then read your store as, `state` and not `state.fooReducer`.
 */
+
+const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+
+export default rootReducer;
